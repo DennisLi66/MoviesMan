5051f657c273ef0e937dd28fdde38834b3cc9426
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2021-03-23 04:03:41 -0700

    Update dbAndTableCreation.sql
    wrote the mysql query that gets all the likes and ratings;
    
    write the wheres that let me get just the one

--- mysql/dbAndTableCreation.sql
+++ mysql/dbAndTableCreation.sql
@@ -33,6 +33,29 @@ rating int NOT NULL,
 CONSTRAINT RL PRIMARY KEY (email,imdbID)
 );
 
+
+-- gets all the likes and ratings
+SELECT * from (
+SELECT ifnull(a.imdbID,c.imdbID) as imdbID, ifnull(Likes,0) as Likes, ifnull(Average,0) as Average FROM 
+(select imdbID,count(*) as Likes from likeList GROUP BY imdbID) a
+LEFT JOIN 
+(select imdbID,avg(rating) as Average from ratingsList GROUP BY imdbID) c ON a.imdbID = c.imdbID
+UNION ALL
+SELECT ifnull(a.imdbID,c.imdbID) as imdbID, ifnull(Likes,0) as Likes, ifnull(Average,0) as Average FROM 
+(select imdbID,count(*) as Likes from likeList GROUP BY imdbID) a RIGHT JOIN 
+(
+select imdbID,avg(rating) as Average from ratingsList GROUP BY imdbID
+) c ON a.imdbID = c.imdbID
+WHERE 
+a.imdbID IS NULL
+) b
+
+
+
+
+
+
+
 -- drop table likeList;
 -- drop table ratingsList;
 -- select * from likeList
